6aaf9ac046f74cedcc6ca7b46b4c60c758c2a8d3
chidiadi <chidiadi-emmanuel-nwosu@gmail.com>
2023-02-16 00:59:10 -0800

    fixed error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,7 +12,8 @@
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 1, 59, stderr);\
+	fwrite("and that piece of art is useful\" - Dora Korpar, \
+2015-10-19\n", 1, 59, stderr);
 
 	return (1);
 }
