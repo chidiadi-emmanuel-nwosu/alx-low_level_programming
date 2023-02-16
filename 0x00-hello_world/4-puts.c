6d55131ca4ea2aa1e727fb635fea4e4fc51c8565
chidiadi <chidiadi-emmanuel-nwosu@gmail.com>
2023-02-15 22:33:22 -0800

    first commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+/*
+*Program that prints
+*Author: Chidiadi E. Nwosu
+*/
+
+#include <stdio.h>
+
+/**
+*main - uses puts function to print
+*
+*Return: 0
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
