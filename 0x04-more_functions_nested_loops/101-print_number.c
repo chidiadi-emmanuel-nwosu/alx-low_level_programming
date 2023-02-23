14af924418f6031c1d346f46449835973fd4b6fe
chidiadi <chidiadi-emmanuel-nwosu@gmail.com>
2023-02-23 08:11:45 -0800

    fixed error

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -18,12 +18,8 @@ void print_number(int n)
 		_putchar('-');
 		n = n * -1;
 	}
-	if ((n / 10) == 0)
-	{
-		_putchar(n + '0');
-		return;
-	}
-	print_number(n / 10);
+	if ((n / 10) > 0)
+		print_number(n / 10);
 
 	_putchar((n % 10) + '0');
 }
