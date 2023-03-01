841129494c2cf7e62ce0b40f61117fa0c9785ba0
chidiadi <chidiadi-emmanuel-nwosu@gmail.com>
2023-03-01 09:35:04 -0800

    fixed error

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -20,6 +20,13 @@ char *_strncpy(char *dest, char *src, int n)
 	while (i < n)
 	{
 		dest[i] = src[i];
+		if (dest[i] == '\0')
+			break;
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
 		i++;
 	}
 	return (dest);
