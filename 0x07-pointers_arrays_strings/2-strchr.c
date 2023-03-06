5ff04c1b149b3df90745d4449b67ce53dd0f441c
chidiadi <chidiadi-emmanuel-nwosu@gmail.com>
2023-03-06 04:01:30 -0800

    fixed error

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -15,10 +15,12 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (1)
 	{
 		if (*(s + i) == c)
 			return (s + i);
+		if (*(s + i) == '\0')
+			break;
 		i++;
 	}
 	return ('\0');
