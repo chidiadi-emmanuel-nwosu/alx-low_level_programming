19e4065ea29901726890f5f2da79611d283ab68c
chidiadi <chidiadi-emmanuel-nwosu@gmail.com>
2023-03-08 03:43:06 -0800

    added 101-wildcmp

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,26 @@
+/*
+ * File: 101-wildcmp.c
+ * Author: Chidiadi Nwosu
+ */
+
+#include "main.h"
+#include <stdio.h>
+/**
+ * wildcmp - compares two strings
+ * @s1: first input string
+ * @s2: second input string
+ *
+ * Return: 1 if s1 and s2 are identical. 0 otherwise
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 != *s2 && *s2 != '*')
+		return (0);
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	return (100);
+}
