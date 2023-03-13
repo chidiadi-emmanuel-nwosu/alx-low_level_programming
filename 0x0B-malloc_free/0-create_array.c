e19b7d71a07f7ba5943cb56106f75a9b1c9aee36
chidiadi <chidiadi-emmanuel-nwosu@gmail.com>
2023-03-13 00:06:56 -0700

    fixed erroe for 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,12 +15,19 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	unsigned int i = 0;
-	char *ptr = (char *)malloc(size);
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		unsigned int i = 0;
+		char *ptr = (char *)malloc(sizeof(char) * size);
 
-	if (ptr == 0)
-		return (0);
-	while (i < size)
-		*(ptr + i++) = c;
-	return (ptr);
+		if (ptr == NULL)
+			return (NULL);
+		while (i < size)
+			*(ptr + i++) = c;
+		return (ptr);
+	}
 }
