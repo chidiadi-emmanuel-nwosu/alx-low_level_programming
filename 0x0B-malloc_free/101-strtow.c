791f30d3a98b31fef2817d2d1faa228079513b47
chidiadi <chidiadi-emmanuel-nwosu@gmail.com>
2023-03-13 21:59:57 -0700

    fixed error for 101-strtow

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -6,72 +6,6 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-/**
- * _strlen - checks the length of a string
- * @str: input string
- *
- * Return: length of str
- */
-int _strlen(char *str)
-{
-	int i = 0;
-
-	while (*(str + i))
-		i++;
-	return (i);
-}
-
-/**
- * _strip - removes unwanted white spaces
- * @str: input string
- * @size: length of input string
- *
- * Return: pointer to copy of input string without spaces
- */
-char *_strip(char *str, int size)
-{
-	int i, j = 0;
-	char *ptr = (char *)malloc(sizeof(char) * size);
-
-	if (str == NULL)
-		return (NULL);
-
-	for (i = 0; *(str + i) != '\0'; i++)
-	{
-		if (i == 0 && *(str + i) == ' ')
-			continue;
-		if (*(str + i) == ' ' && *(str + i - 1) == ' ')
-			continue;
-		*(ptr + j) = *(str + i);
-		j++;
-	}
-	if (*(ptr + j - 1) != ' ')
-		*(ptr + j) = '\0';
-	else
-		*(ptr + j - 1) = '\0';
-
-	return (ptr);
-}
-
-
-/**
- * check_height - checks the height of 2D array
- * @str: input string
- *
- * Return: height of the array
- */
-int check_height(char *str)
-{
-	int i = 0, j = 0;
-
-	while (*(str + i))
-	{
-		if (str[i] == ' ')
-			j++;
-		i++;
-	}
-	return (j + 1);
-}
 
 /**
  * strtow - splits a string into words
@@ -81,43 +15,44 @@ int check_height(char *str)
  */
 char **strtow(char *str)
 {
-	char **p;
-	char *ptr;
-	int i, j = 0, k = 0, len, height;
+	char **ptr;
+	int i, j = 0, row = 0, col, width = 0, height = 0;
 
-	ptr = _strip(str, _strlen(str));
-	len = _strlen(ptr);
-	height = check_height(ptr);
-	p = malloc(sizeof(char *) * (height + 1));
-	if (p == NULL || len <= 1)
+	for (i = 0; str[i] != '\n'; i++)
+	{
+		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
+			height++;
+	}
+	if (str == NULL || height == 0)
 		return (NULL);
 
-	for (i = 0; i <= len; i++)
-	{
-		k++;
-		if (ptr[i] == ' ' || i == len)
-		{
-			int l, m = 0;
+	ptr = malloc(sizeof(*ptr) * (height + 1));
+	if (ptr == NULL)
+		return (NULL);
 
-			p[j] = malloc(sizeof(char) * k);
-			if (p[j] == NULL)
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] != ' ')
+			width++;
+		if ((i > 0 && str[i] == ' ' && str[i - 1] != ' ') ||
+				(str[i] != ' ' && str[i + 1] == '\0'))
+		{
+			ptr[row] = malloc(sizeof(char) * width + 1);
+			if (ptr[row] == NULL)
 			{
-				for (; j >= 0; j--)
-					free(p[j]);
-				free(p);
+				for (; row >= 0; row--)
+					free(ptr[row]);
+				free(ptr);
 				return (NULL);
 			}
-			else
-			{
-				for (l = (i + 1 - k); l < i; l++)
-					p[j][m++] = ptr[l];
-				p[j][l] = '\0';
-			}
-			j++;
-			k = 0;
+			col = 0;
+			for (j = i - width; j < i; j++)
+				ptr[row][col++] = str[j];
+			ptr[row][col] = '\0';
+			row++;
+			width = 0;
 		}
 	}
-	p[j] = NULL;
-	free(ptr);
-	return (p);
+	ptr[row] = NULL;
+	return (ptr);
 }
