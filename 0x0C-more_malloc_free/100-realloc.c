7c840b2e536e42ef31968ed298ead2d92d17830c
Chidiadi Emmanuel Nwosu <chidiadi-emmanuel-nwosu@gmail.com>
2023-03-16 14:02:53 +0100

    fixed errors for 100-realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -8,7 +8,7 @@
 
 /**
  * _realloc - reallocates a memory block using malloc and free
- * @ptr: pointer to the memory previously allocated 
+ * @ptr: pointer to the memory previously allocated
  * @old_size: the size, in bytes, of the allocated space for ptr
  * @new_size: the new size, in bytes of the new memory block
  *
@@ -18,7 +18,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *p;
 	unsigned int i = 0;
-	
+
 	if (new_size <= old_size)
 	{
 		return (ptr);
@@ -39,6 +39,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	else
 	{
 		char *cast_p, *cast_ptr;
+
 		p = malloc(new_size);
 		if (p == NULL)
 			return (NULL);
