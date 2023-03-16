b534f7e4133de20f4bdee03e810945bd3af3230a
Chidiadi Emmanuel Nwosu <chidiadi-emmanuel-nwosu@gmail.com>
2023-03-16 13:38:57 +0100

    fixed errors for 2-calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,14 +15,18 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr = NULL;
+	unsigned int i;
+	char *ptr = NULL;
 
 	if (nmemb <= 0 || size <= 0)
 		return (NULL);
 
-	ptr = malloc(sizeof(size) * nmemb);
+	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
+	for (i = 0; i < nmemb * size; i++)
+		ptr[i] = 0;
+	ptr = (void *)ptr;
 
 	return (ptr);
 }
