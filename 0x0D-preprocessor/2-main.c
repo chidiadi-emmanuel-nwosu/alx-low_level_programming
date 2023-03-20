beb5276a2d46c1b4e47572598383805a0ceab7d9
Chidiadi Emmanuel Nwosu <chidiadi-emmanuel-nwosu@gmail.com>
2023-03-20 08:30:12 +0100

    first project commit

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,18 @@
+/*
+ * File: 2-main.c
+ * Author: Chidiadi E. Nwosu
+ */
+
+#include <stdio.h>
+
+/**
+ * main - program that prints the name of the file it was
+ *        compiled from, followed by a new line.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
