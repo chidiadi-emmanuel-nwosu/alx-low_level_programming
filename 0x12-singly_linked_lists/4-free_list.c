786cd5e32b21e7673e80ea270f6a5a7f43eeba5d
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-03-30 14:33:12 +0100

    added 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,24 @@
+/*
+ * File:  4-free_list.c
+ * Author: Chidiadi E. Nwosu
+ */
+
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list.
+ * @head: pointer to the head node
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
