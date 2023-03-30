030b8e416811b941ab47b473836ee0511e55e6f1
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-03-30 15:34:23 +0100

    fixed error for 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,12 +6,14 @@
 #include "lists.h"
 
 /**
- * exec_before_main - function that executes before the main function is executed
+ * exec_before_main - function that executes before
+ *                    the main function is executed
  *
  * Return: void
  */
 
-void __attribute__((constructor)) exec_before_main(void) 
+void __attribute__((constructor)) exec_before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
