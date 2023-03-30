14397af8c0c71075e40b4be3a5e369bec127624d
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-03-30 15:13:14 +0100

    added 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,17 @@
+/*
+ * File: 100-first.c
+ * Author: Chidiadi E. Nwosu
+ */
+
+#include "lists.h"
+
+/**
+ * exec_before_main - function that executes before the main function is executed
+ *
+ * Return: void
+ */
+
+void __attribute__((constructor)) exec_before_main(void) 
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
