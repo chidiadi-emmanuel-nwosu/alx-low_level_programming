29c72eb7f284f37b2aff5ba23346bf918c5e9c55
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-04-03 22:10:52 +0100

    added 100-reverse_listint.c advanced task

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,32 @@
+/*
+ * File: 100-reverse_listint.c
+ * Author: Chidiadi E. Nwosu
+ */
+
+#include "lists.h"
+
+/**
+ * reverse_listint - reverse a listint_t linked list.
+ * @head: pointer to the head node
+ *
+ * Return: the head node on success, NULL on failure.
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev_node = NULL, *next_node = *head;
+
+	if (*head == NULL)
+		return (NULL);
+
+	while (*head)
+	{
+		next_node = (*head)->next;
+		(*head)->next = prev_node;
+		prev_node = *head;
+		*head = next_node;
+	}
+
+	*head = prev_node;
+
+	return (*head);
+}
