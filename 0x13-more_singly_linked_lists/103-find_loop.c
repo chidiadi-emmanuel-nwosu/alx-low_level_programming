5598ccffe3213ccc37aba36effc9eb0c3f3d6ec6
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-04-04 23:14:28 +0100

    fixed requirement error for 103-find_list_loop

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -5,7 +5,6 @@
 
 #include "lists.h"
 
-size_t check_loop3(listint_t *head);
 /**
  * find_listint_loop - find the loop in a linked list.
  * @head: pointer to the head node
@@ -15,33 +14,10 @@ size_t check_loop3(listint_t *head);
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	size_t count = 0, i = 0;
-
-	count = check_loop3(head);
-
-	if (count == 0)
-		return (NULL);
-
-	for (; i < count; i++)
-		head = head->next;
-	return (head);
-}
-
-
-
-/**
- * check_loop3 - checks for loop in a linked listint_t.
- * @head: head node input.
- *
- * Return: the number of node in the list
- */
-size_t check_loop3(listint_t *head)
-{
-	size_t count = 1, check = 0;
 	listint_t *slow, *fast;
 
 	if (head == NULL || head->next == NULL)
-		return (0);
+		return (NULL);
 
 	slow = head->next;
 	fast = head->next->next;
@@ -50,34 +26,19 @@ size_t check_loop3(listint_t *head)
 	{
 		if (slow == fast)
 		{
-			check = 1;
-			break;
+			slow = head;
+
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (slow);
 		}
 		slow = slow->next;
 		fast = fast->next->next;
 	}
 
-	if (check == 0)
-	{
-		return (0);
-	}
-	else
-	{
-		slow = head;
 
-		while (slow != fast)
-		{
-			count += 1;
-			slow = slow->next;
-			fast = fast->next;
-		}
-		slow = slow->next;
-		while (fast != slow)
-		{
-			count += 1;
-			slow = slow->next;
-		}
-	}
-
-	return (count);
+	return (NULL);
 }
