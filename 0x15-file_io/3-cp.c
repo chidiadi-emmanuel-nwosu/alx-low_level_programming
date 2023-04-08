f5bf4d6aa0ff79db53a828462f192b85ef2ca693
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-04-08 17:32:18 +0100

    added 3-cp.c file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,72 @@
+/*
+ * File: 3-cp.c
+ * Author: Chidiadi E. Nwosu
+ */
+
+#include "main.h"
+
+/**
+ * main - program that copies the content of a file to another file.
+ * @ac: number of commandline arguments.
+ * @av: array of commandline arguments.
+ *
+ * Return: 0 on success.
+ */
+int main(int ac, char **av)
+{
+	int file_from, file_to, re, wr;
+	char buffer[1024];
+
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", av[0]);
+		exit(97);
+	}
+
+	file_from = open(av[1], O_RDONLY);
+	if (file_from < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+
+	file_to = open(av[2], O_CREAT | O_TRUNC | O_WRONLY, 00664);
+	if (file_to < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+
+	do {
+		re = read(file_from, buffer, 1024);
+
+		wr = write(file_to, buffer, re);
+		if (wr < 0)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
+	} while (re > 0);
+
+	close_fd(file_from);
+	close_fd(file_to);
+
+	return (0);
+}
+
+
+
+/**
+ * close_fd - closes an open file descriptor.
+ * @fd: file descriptor input.
+ *
+ * Return: void
+ */
+void close_fd(int fd)
+{
+	if (close(fd) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd);
+		exit(100);
+	}
+}
