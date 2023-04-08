e0cd04b389f95225ff417049f0029eb376e74a32
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-04-08 18:46:52 +0100

    fixed error

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,14 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
+#include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
 
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void close_fd(int fd);
+char *init_buffer(void);
 
 
 #endif
