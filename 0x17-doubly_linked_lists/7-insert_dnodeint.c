e14fa93694ca078a4944910e42c7be52a4f09474
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-05-18 12:49:01 +0100

    fixed error for task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -48,7 +48,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		{
 			new->prev = NULL;
 			new->next = NULL;
-			tmp = new;
+			*h = new;
 		}
 		return (new);
 	}
