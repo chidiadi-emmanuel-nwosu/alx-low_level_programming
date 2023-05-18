ac6ce744dc90f29a318220f7ce140c1a5f80f203
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-05-18 12:33:47 +0100

    fixed error 2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -16,9 +16,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new = NULL;
 
-	if (!head)
-		return (NULL);
-
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
 		return (NULL);
