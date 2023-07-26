ef84936511e59640d935754dc533cb996714ab6a
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-07-26 13:00:11 +0100

    my first commit

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    perimeter module
+"""
+
+
+def island_perimeter(grid):
+    """that returns the perimeter of
+       the island described in grid
+    """
+    check = 0
+    perimeter = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if j == 1 and check == 0:
+                perimeter += 4
+                check = 1
+            elif j == 1 and check == 1:
+                perimeter += 2
+
+    return perimeter
+
